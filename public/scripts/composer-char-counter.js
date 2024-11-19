0f6e54c2b94f432a1fa2d016ba0a644a2c2c54c0
semblant <robynhumphreys7_4@hotmail.com>
2024-11-18 22:45:18 -0700

    Update character counter to use JQuery

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,28 +1,20 @@
 $(document).ready(function() {
-  console.log('working!')
-
-  // Select the form element
-  const tweetForm = document.querySelector("#tweet-form");
-
   // Add event listener to update the character counter on input
-  tweetForm.addEventListener('input', function(event) {
-  event.preventDefault();
+  $("#tweet-form").on('input', function() {
+    const maxChar = 140;
+    const charsTyped = $("#tweet-text").val().length; // Get current text from textarea element
+    let remainingChars = maxChar - charsTyped;
 
-  const maxChar = 140;
-  const charsTyped = event.target.value; // Get current text from textarea element
-  const outputElement = this.elements.counter // Access output element in DOM
-  let remainingChars = maxChar - charsTyped.length;
+    // Update the output element with remaining character count
+    const $counter = $(this).find(".counter"); // Access output element in DOM
+    $counter.val(remainingChars);
 
-  // Update the output element with remaining character count
-  outputElement.value = remainingChars;
-
-  // Change Colour based on remaining characters
-  if (remainingChars < 0 ) {
-    outputElement.style.color = "red";
-  } else {
-    outputElement.style.color = "#a49e96"
-  }
-
+    // Change Colour based on remaining characters
+    if (remainingChars < 0 ) {
+      $counter.css("color", "red");
+    } else {
+      $counter.css("color", "#a49e96");
+    }
   });
 });
 
