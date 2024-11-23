c35e7adc32baf349286203734780b799a7370857
semblant <robynhumphreys7_4@hotmail.com>
2024-11-22 22:42:02 -0700

    Call loadTweets function to get tweet after submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,8 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
-  /**
+ /**
    * Function takes a tweet object and returns the HTML for the object
    *
    * @param {Object} tweet - the tweet to create the HTML for
@@ -13,55 +12,91 @@ $(document).ready(function() {
    * @returns tweet in HTML format (<article>)
    */
 
-  const createTweetElement = function(tweet) {
-    /*
-      In your createTweetElement function, you're directly assigning the values from the tweet object to variables. While this works, it might be cleaner to use destructuring to extract the values. For example, you could write:
+ const createTweetElement = function(tweet) {
+  /*
+    In your createTweetElement function, you're directly assigning the values from the tweet object to variables. While this works, it might be cleaner to use destructuring to extract the values. For example, you could write:
 
-      const { name: userName, avatars: userAvatar, handle: userHandle } = tweet.user;
-      const { text: tweetContent } = tweet.content;
-    */
+    const { name: userName, avatars: userAvatar, handle: userHandle } = tweet.user;
+    const { text: tweetContent } = tweet.content;
+  */
 
-    // Store info from the tweet
-    const userName = tweet.user.name;
-    const userAvatar = tweet.user.avatars;
-    const userHandle = tweet.user.handle;
-    const tweetContent = tweet.content.text;
-    const tweetCreateDate = tweet.created_at;
+  // Store info from the tweet
+  const userName = tweet.user.name;
+  const userAvatar = tweet.user.avatars;
+  const userHandle = tweet.user.handle;
+  const tweetContent = tweet.content.text;
+  const tweetCreateDate = tweet.created_at;
 
-    // Create HTML variable
-    const $tweet = `<article class='tweet'>
-        <header>
-          <span class="tweet-name">
-            <img class="avatar" src=${userAvatar}/> ${userName}
-          </span>
-          <h3>${userHandle}</h3>
-        </header>
-        <section class='tweet-text'>${tweetContent}</section>
-        <footer>
-          <span> ${timeago.format(tweetCreateDate)}</span>
-          <div class='tweet-icons'>
-            <i name='retweet' class='fas fa-retweet'></i>
-            <i name='flag' class='fas fa-flag'></i>
-            <i name='heart' class='fas fa-heart'></i>
-          </div>
-        </footer>
-      </article>`;
-    return $tweet;
-  };
+  // Create HTML variable
+  const $tweet = `<article class='tweet'>
+      <header>
+        <span class="tweet-name">
+          <img class="avatar" src=${userAvatar}/> ${userName}
+        </span>
+        <h3>${userHandle}</h3>
+      </header>
+      <section class='tweet-text'>${tweetContent}</section>
+      <footer>
+        <span> ${timeago.format(tweetCreateDate)}</span>
+        <div class='tweet-icons'>
+          <i name='retweet' class='fas fa-retweet'></i>
+          <i name='flag' class='fas fa-flag'></i>
+          <i name='heart' class='fas fa-heart'></i>
+        </div>
+      </footer>
+    </article>`;
+  return $tweet;
+};
 
 /**
- *  Function passes each tweet in the database to createTweetElement() and appends the return value to the HTML container
- *
- * @param {Object} tweets - database of tweets
- */
+*  Function passes each tweet in the database to createTweetElement() and appends the return value to the HTML container
+*
+* @param {Object} tweets - database of tweets
+*/
 const renderTweets = function(tweets) {
-  // Loop through each tweet in the DB
-  for (const $tweet of tweets) {
-    const $tweetElement =  createTweetElement($tweet); // get HTML format of tweet
-    $('.tweets-container').prepend($tweetElement); // append tweet in HTML
-  }
+// Loop through each tweet in the DB
+for (const $tweet of tweets) {
+  const $tweetElement =  createTweetElement($tweet); // get HTML format of tweet
+  $('.tweets-container').prepend($tweetElement); // append tweet in HTML
+}
 }
 
+
+/**
+ * Function that makes a request to '/', grabs the results and passes them to the renderTweets function
+ */
+const loadTweets = function() {
+  $.get('/tweets')
+  .then((tweets) => { // after successful get request
+    renderTweets(tweets)
+  })
+  .catch((e) => { // catch any errors
+    alert(e);
+  })
+}
+
+
+/**
+* Function that takes in a tweet and validates that it is less than 140 characters and is not blank.
+*
+* @param {string} tweet - the tweet to validate
+* @returns
+*        - false: tweet either is > 140 chars or is blank
+*        - true: tweet is valid
+*/
+const validateTweet = function(tweet) {
+if (!tweet) {
+  return alert("Tweet cannot be blank!");
+}
+if (tweet.length > 140) {
+  return alert('Cannot tweet more than 140 characters');
+}
+return true;
+}
+
+
+// Requests the tweets path to render tweets and handles tweet submission
+$(document).ready(function() {
   // Add event listener to send tweet data to server on submit
   $('#tweet-form').on('submit', function(event) {
     event.preventDefault();
@@ -74,37 +109,20 @@ const renderTweets = function(tweets) {
     const url = $form.attr("action");
 
     // Validation
-    if (!content) {
-      return alert("Tweet cannot be blank!");
-    }
-
-    if (content.length > 140) {
-      return alert('Cannot tweet more than 140 characters');
-    }
+    if (!validateTweet(content)) return;
 
     // Send the data using post
     $.post(url, { text: content })
     .catch((err) => {
-      alert(err) // doesn't work?
+      alert(err);
+    })
+    .done(() => {
+      loadTweets(); // load newest tweet
     });
   });
 
-  /**
-   * Function that makes a request to '/', grabs the results and passes them to the renderTweets function
-   */
-  const loadTweets = function() {
-    $.get('/tweets')
-    .then((tweets) => { // after successful get request
-      renderTweets(tweets)
-    })
-    .catch((e) => { // catch any errors
-
-    })
-  }
-
   loadTweets();
 });
-
 
 
 
